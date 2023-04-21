c331c89d60d25a6458e320ea0886173d6c683524
aliiahmadi <aliahmadi.github@gmail.com>
2023-04-21 13:15:24 +0330

    improve

--- tests/Test.php
+++ tests/Test.php
@@ -4,14 +4,17 @@ use PHPUnit\Framework\TestCase;
 
 class Test extends TestCase
 {
+
+    private static $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiYWxpIiwiZmFtaWx5IjoiYWhtYWRpIiwiZXhwIjoxOTk3NDIxNjc1fQ.1m7nzPusjSQqOZ4Ret645R_wdD89nhHcncUk5HgzxeU";
+
+
     public function test_get_successfuly()
     {
 
-        $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiYWxpIiwiZmFtaWx5IjoiYWhtYWRpIiwiZXhwIjoxOTk3NDIxNjc1fQ.1m7nzPusjSQqOZ4Ret645R_wdD89nhHcncUk5HgzxeU";
         $client = new GuzzleHttp\Client([
             'base_uri' => 'http://localhost:5000/api',
             'headers' => [
-                'Authorization' => 'Bearer ' . $token
+                'Authorization' => 'Bearer ' . self::$token
             ]
         ]);
         $response = $client->get("inventory");
@@ -29,11 +32,10 @@ class Test extends TestCase
 
     public function test_get_by_id()
     {
-        $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiYWxpIiwiZmFtaWx5IjoiYWhtYWRpIiwiZXhwIjoxOTk3NDIxNjc1fQ.1m7nzPusjSQqOZ4Ret645R_wdD89nhHcncUk5HgzxeU";
         $client = new GuzzleHttp\Client([
             'base_uri' => 'http://localhost:5000/api',
             'headers' => [
-                'Authorization' => 'Bearer ' . $token
+                'Authorization' => 'Bearer ' . self::$token
             ]
         ]);
 
@@ -43,11 +45,10 @@ class Test extends TestCase
 
     public function test_register()
     {
-        $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiYWxpIiwiZmFtaWx5IjoiYWhtYWRpIiwiZXhwIjoxOTk3NDIxNjc1fQ.1m7nzPusjSQqOZ4Ret645R_wdD89nhHcncUk5HgzxeU";
         $client = new GuzzleHttp\Client([
             'base_uri' => 'http://localhost:5000/api',
             'headers' => [
-                'Authorization' => 'Bearer ' . $token,
+                'Authorization' => 'Bearer ' . self::$token,
                 'Content-Type' => 'application/json'
             ]
         ]);
